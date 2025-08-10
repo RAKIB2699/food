d66656bd32c698cf8d5e61d0d46d629d49913059
RAKIB2699 <rakibhasan01645172699@gmail.com>
2025-08-10 20:07:44 +0600

    feature section polish

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import img1 from '../assets/foodimg1.avif';
 import img2 from '../assets/foodimg2.avif';
 import img3 from '../assets/foodimg3.avif';
 
 const Banner = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll('.carousel a[href^="#slide"]');
+    function handleClick(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElem = document.getElementById(targetId);
+      if (targetElem) {
+        targetElem.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+      }
+    }
+    links.forEach(link => link.addEventListener('click', handleClick));
+    return () => {
+      links.forEach(link => link.removeEventListener('click', handleClick));
+    };
+  }, []);
+
   return (
-   <div className='w-11/12 mx-auto mt-6'>
-     <div className="carousel  h-[75vh] rounded-lg overflow-hidden shadow-md">
-    
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full object-cover" alt="Slide 1" />
-      
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
+    <div className='h-30vh max-w-[1600px] w-11/12 mx-auto mt-6'>
+      <div className="carousel  h-[60vh]  rounded-lg overflow-hidden shadow-md">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} className="w-full object-cover" alt="Slide 1" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
         </div>
-      </div>
 
-     
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full object-cover" alt="Slide 2" />
-      
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full object-cover" alt="Slide 2" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
         </div>
-      </div>
 
-      
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} className="w-full object-cover" alt="Slide 3" />
-       
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full object-cover" alt="Slide 3" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -18,76 +18,81 @@ const Navbar = () => {
 
   const navLinks = (
     <>
-      <li><NavLink to="/" className="hover:text-green-400">Home</NavLink></li>
-      <li><NavLink to="/availablefood" className="hover:text-green-400">Available Foods</NavLink></li>
-      <li><NavLink to="/addfood" className="hover:text-green-400">Add Food</NavLink></li>
-      <li><NavLink to="/manage-food" className="hover:text-green-400">Manage My Foods</NavLink></li>
-      <li><NavLink to="/myfoodrequest" className="hover:text-green-400">My Food Request</NavLink></li>
+      <li><NavLink to="/" className="hover:text-green-400 hover:font-bold text-black">Home</NavLink></li>
+      <li><NavLink to="/availablefood" className="hover:text-green-400 hover:font-bold text-black">Available Foods</NavLink></li>
+      <li><NavLink to="/addfood" className="hover:text-green-400 hover:font-bold text-black">Add Food</NavLink></li>
+      <li><NavLink to="/manage-food" className="hover:text-green-400 hover:font-bold text-black">Manage My Foods</NavLink></li>
+      <li><NavLink to="/myfoodrequest" className="hover:text-green-400 hover:font-bold text-black">My Food Request</NavLink></li>
     </>
   );
 
   return (
-    <div className="navbar bg-gray-800 text-white w-11/12 mx-auto shadow-md rounded-md px-4">
+    <div className="navbar text-white backdrop-blur-sm shadow-sm sticky top-0 z-50 p-0">
 
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+      <div className="navbar max-w-[1600px] w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-black"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black rounded-box w-52">
+              {navLinks}
+            </ul>
+            <Link to="/" className="btn btn-ghost text-xl text-green-500">Foody</Link>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black rounded-box w-52">
+
+        </div>
+
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
             {navLinks}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost text-xl text-green-500">Foody</Link>
-      </div>
 
 
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {navLinks}
-        </ul>
-      </div>
+        <div className="navbar-end">
+
+          {user ? (
+            <div className="flex items-center gap-4 relative group">
+
+              <img
+                src={user.photoURL}
+                alt="User"
+                className="w-10 h-10 rounded-full border cursor-pointer"
+              />
 
 
-      <div className="navbar-end">
-
-        {user ? (
-          <div className="flex items-center gap-4 relative group">
-
-            <img
-              src={user.photoURL}
-              alt="User"
-              className="w-10 h-10 rounded-full border cursor-pointer"
-            />
+              <div className="absolute top-12 right-0 bg-black text-white text-sm px-3 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-200 z-50">
+                {user.displayName || ''}
+              </div>
 
 
-            <div className="absolute top-12 right-0 bg-black text-white text-sm px-3 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-200 z-50">
-              {user.displayName || ''}
+              <button onClick={handleLogout} className="btn btn-sm bg-primary text-white">
+                Logout
+              </button>
             </div>
+          ) : (
+            <NavLink to="/login" className="btn">Log in</NavLink>
+          )}
 
-
-            <button onClick={handleLogout} className="btn btn-sm bg-red-500 hover:bg-red-600 text-white">
-              Logout
-            </button>
-          </div>
-        ) : (
-          <NavLink to="/login" className="btn">Log in</NavLink>
-        )}
-
+        </div>
       </div>
     </div>
   );
